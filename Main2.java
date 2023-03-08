8dbdb73f59190146c63ed82bf68b8748a91cd9d0
LuisaCelis17 <125531418+LuisaCelis17@users.noreply.github.com>
2023-03-08 12:22:22 -0400

    Add files via upload

---
+++ Main2.java
@@ -0,0 +1,23 @@
+package TaskJava1;
+
+import java.sql.SQLOutput;
+
+/*Para este ejercicio tendréis que crear una función que reciba un precio y
+devuelva el precio con el IVA incluido.*/
+public class Main2 {
+    public static void main(String [] args){
+        System.out.println("Programa que incluye 16% de IVA");
+        float precio = 5211.6f;
+        float precioFinal;
+        float iva;
+
+        iva = (16 * precio) /100;
+        precioFinal = precio + iva;
+
+        System.out.println("El precio original es " + precio);
+        System.out.println("El IVA que se incluye es de 16% del costo del producto, que seria " + iva);
+        System.out.println("El precio final es " + precioFinal);
+
+
+    }
+}
